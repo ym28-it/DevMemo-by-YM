d28b8b3db4539562d856f65df15d891a18cb4211
ym28-it <120317084+ym28-it@users.noreply.github.com>
2025-01-30 15:16:07 +0900

    blog

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
-// aside.html を非同期で読み込む
-document.addEventListener("DOMContentLoaded", () => {
-    const repoName = "DevMemo-by-YM"
-    const asidePath = `${window.location.origin}/${repoName}/aside.html`
-    fetch(asidePath)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("aside-container").innerHTML = data;
-            setupSideMenu(); // サイドメニューのスクリプトを適用
-        })
-        .catch(error => console.error("Error loading aside:", error));
-});
 
-// サイドメニューのイベントリスナーを設定する関数
-function setupSideMenu() {
+// side-menu
+document.addEventListener("DOMContentLoaded", () => {
     const sidebarToggle = document.getElementById("sidebarToggle");
     const sideMenu = document.getElementById("sideMenu");
 
-    if (sidebarToggle && sideMenu) {
-        sidebarToggle.addEventListener("click", () => {
-            sideMenu.classList.toggle("open");
-        });
-    }
+    sidebarToggle.addEventListener("click", () => {
+        sideMenu.classList.toggle("open");
+    });
+});
 
+// toggle in side-menu
+document.addEventListener("DOMContentLoaded", () => {
     const toggleButtons = document.querySelectorAll(".side-menu .toggle");
+
     toggleButtons.forEach((button) => {
         button.addEventListener("click", () => {
             const parentLi = button.parentElement;
             const subMenu = parentLi.querySelector("ul");
 
+            // 子要素の表示・非表示を切り替え
             if (subMenu) {
                 subMenu.style.display = subMenu.style.display === "none" || subMenu.style.display === "" ? "block" : "none";
-                parentLi.classList.toggle("open");
+                parentLi.classList.toggle("open"); // クラスの切り替えでアイコン調整
             }
         });
     });
-}
-
+});
